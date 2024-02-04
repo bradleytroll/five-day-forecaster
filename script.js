24ae853a11c3be36e43bd05bdb4b084fbd675560
Bradley Troll <bradleytroll@gmail.com>
2024-02-04 10:59:17 -0500

    Creates fetch request that makes a successful call using API key.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var cityForm = document.getElementById('city-form');
 var cityInput = document.getElementById('city-input');
 var weatherData = document.getElementById('weather-data');
 var forecast = document.getElementById('forecast');
+var apiKey = "d7d2daaf620c75d9e65a81dfbee535d2"
+
 
 // Adds an event listener to the form. Upon submit, a variable is created based on user input, which is then trimmed from spacing and altered to all lower case letters. 
 cityForm.addEventListener('submit', function(e) {
@@ -15,5 +17,17 @@ cityForm.addEventListener('submit', function(e) {
 
 // (in progress) Creates function to get weather data.
 function getWeatherData(city) {
-    console.log("hi")
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.log('Error fetching weather data:', error);
+        });
+}
+
+function displayWeather(data) {
+    console.log(data)
 }
