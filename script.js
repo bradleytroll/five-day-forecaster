610f9b070c9eb60a9d2ffb3b9fdd9de7a3e30747
Bradley Troll <bradleytroll@gmail.com>
2024-02-04 10:37:28 -0500

    Declares variables in Javascript.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Declarations: Grabs html and assigns variables or creates new elements.
+
+var cityForm = document.getElementById('city-form');
+var cityInput = document.getElementById('city-input');
+var weatherData = document.getElementById('weather-data');
+var forecast = document.getElementById('forecast');
+
 
