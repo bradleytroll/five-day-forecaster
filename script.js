34a126f0ca548a8749ab115943824abae3e56423
Bradley Troll <bradleytroll@gmail.com>
2024-02-04 10:42:49 -0500

    Adds event listener to the form that fires a function to retrieve weather data. The retrieval function is incomplete.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Declarations: Grabs html and assigns variables or creates new elements.
-
 var cityForm = document.getElementById('city-form');
 var cityInput = document.getElementById('city-input');
 var weatherData = document.getElementById('weather-data');
 var forecast = document.getElementById('forecast');
 
+// Adds an event listener to the form. Upon submit, a variable is created based on user input, which is then trimmed from spacing and altered to all lower case letters. 
+cityForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var cityName = cityInput.value.trim().toLowerCase();
+    if (cityName) {
+        getWeatherData(cityName);
+    }
+})
 
+// (in progress) Creates function to get weather data.
+function getWeatherData(city) {
+    console.log("hi")
+}
